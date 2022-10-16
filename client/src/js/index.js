84fa9974c881e98a75ce967e799a0fa0b16e70a4
Amanda Perry <challenge641@gmail.com>
2022-10-16 10:51:19 -0500

    rendering card functionality

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,5 +1,4 @@
 import "./form";
-import "./submit";
 
 import "../css/index.css";
 
@@ -9,11 +8,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
+
 import { getDb, initDb, postDb } from "./database";
+import { fetchCards } from "./cards";
+import { toggleForm, clearForm } from "./form"
 
 
 window.addEventListener('load', function () {
   initDb();
+  fetchCards();
   // getDb();
   // postDb("IamtheWURST", "iamthewurst@gmail.com", 4438483223, "Bear");
   document.getElementById('logo').src = Logo;
@@ -21,3 +24,38 @@ window.addEventListener('load', function () {
   document.getElementById('dogThumbnail').src = Dog;
 });
 
+// Form functionality
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener('click', event => {
+  toggleForm()
+})
+
+form.addEventListener('submit', event => {
+  // Handle data
+  event.preventDefault();
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+  let profile = document.querySelector('input[type="radio"]:checked').value;
+
+  // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+  if (submitBtnToUpdate == false) {
+    postDb(name, email, phone, profile);
+  } else {
+
+    fetchCards();
+    // Toggles the submit button back to POST functionality
+    submitBtnToUpdate = false;
+  }
+
+  // Clear form
+  clearForm();
+  // Toggle form
+  toggleForm();
+  // Reload the DOM
+  fetchCards();
+});
